3b92a728b256387e57e87f76d6fa550470a72f78
James Correia <jcorr851@gmail.com>
2022-05-05 01:20:38 -0400

    General improvements -Enclosed free code in a main() function -added shebang -removed constants in favor of default values

--- benford.py
+++ benford.py
@@ -1,19 +1,28 @@
+#!/usr/bin/env python3
 import random
 import math
 
-NUM_DATAPOINTS = 100    #Size of the generated data set
-NUM_SAMPLES = 100000    #Make larger to increase bias towards low first digits.
-START_POINT = 10000     #Does not affect distribution, must be larger on high sample counts 
-STARTING_BASE = 2       #First base to test the distribution on
-ENDING_BASE = 16        #Last base to test the distribution on
 VERBOSE = False
 
+#param: starting_base lowest base to test the distribution on
+#param: ending_base greatest base to test the distribution on
+def main(starting_base = 2, ending_base = 16):    
+    #Generate datapoints
+    datapoints = generate_benford_distribution()
+
+    #for each base desired print out a digit frequency table
+    for base in range(starting_base, ending_base + 1): #+1 because range() is exlusive on the final value
+        analyze_benford_distribution(datapoints, base)
+
 #Starting at START_POINT, successively multiply by a random number between 0.99 and 1.01 NUM_SAMPLES times to produce one datapoint, repeat for each datapoint desired.
-def generate_benford_distribution(num_datapoints, num_samples):
+#param num_datapoints: size of the generated data set
+#param num_samples: Make larger to increase bias towards low first digits
+#param start_point: Does not affect distribution, must be larger on high sample counts however
+def generate_benford_distribution(num_datapoints = 100, num_samples = 100000, start_point = 10000):
     datapoints = [0] * num_datapoints 
    
     for i in range(num_datapoints):
-        value = START_POINT
+        value = start_point
         for j in range(num_samples):
             sample = random.uniform(0.99, 1.01)
             value = value * sample
@@ -24,6 +33,7 @@ def generate_benford_distribution(num_datapoints, num_samples):
 
         datapoints[i] = math.floor(value)
 
+    #print out the entire dataset
     if (VERBOSE) :
         for i in range(len(datapoints)):
             print(datapoints[i])
@@ -46,11 +56,6 @@ def analyze_benford_distribution(datapoints, radix):
         occurances = digit_occurances[i]
         print("{}:\t{}".format(i, occurances))
         
-    
-#Generate datapoints
-datapoints = generate_benford_distribution(NUM_DATAPOINTS, NUM_SAMPLES)
 
-#for each base desired print out a digit frequency table
-for base in range(STARTING_BASE, ENDING_BASE + 1): #+1 because range() is exlusive on the final value
-    analyze_benford_distribution(datapoints, base)
-
+if (__name__ == "__main__"):
+    main()
